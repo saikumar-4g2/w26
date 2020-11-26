41b441058b6f961b61ed1f6c7720a2e1a9b08218
saikumar-4g2 <72550841+saikumar-4g2@users.noreply.github.com>
2020-11-26 12:27:42 +0530

    Update test_cafeteria.c

--- test/test_cafeteria.c
+++ test/test_cafeteria.c
@@ -1,14 +1,12 @@
 #include <CUnit/Basic.h>
 #include <CUnit/CUnit.h>
 
-/* Modify these two lines according to the project */
-#include <stdarg.h>
-#include <cafeteria.h>
-#define PROJECT_NAME    "CafeteriaRating"
+#include <patientanalysis.h>
+#define PROJECT_NAME "patientanalysis"
 
 /* Prototypes for all the test functions */
-void test_cafeteria(void)
-//void test_mylib1(void)
+void test_do_patientanalysis(void);
+
 
 /* Start of the application test */
 int main() {
@@ -19,10 +17,9 @@ int main() {
   CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
 /* Note: Do not edit END */
 
+/* Add your test functions in this format for testing*/
+  CU_add_test(suite, "patientanalysis", test_do_patientanalysis);
 
-  /* Add your test functions in this format for testing*/
-  CU_add_test(suite, "mylib", test_mylib);
- // CU_add_test(suite, "mylib1", test_mylib1);
 
 
 /* Note: Do not edit START*/
@@ -37,38 +34,9 @@ int main() {
 /* Note: Do not edit END */
   return 0;
 }
-
-
-
 /* Write all the test functions */
+void test_do_patientanalysis(void) {
+  CU_ASSERT_EQUAL(1,avg_rating(180));
 
-
-
-/*float overall_rating(int count,...)    //helps to take variable number of arguments
-{
-    int i=0;
-    //int frequency_array[10]={};   //To know the no of students given a particular scale
-    int rating_array[40]={};     //To store individual ratings given by the 40 students
-    int total_rating=0;          // A variable to store the total rating given 40 students
-    //float average_rating=0;     //Variable to store average overall rating of cafeteria
-    va_list lst;                //variable of va_list defined in stdarg.h header file
-    va_start(lst,count);        // va_start is a macro to intialize va_list
-    for(i=0;i<count;i++)
-    {
-         rating_array[i]=va_arg(lst,int);   //va_arg
-         while(rating_array[i]<1||rating_array[i]>max_rating)
-         {
-             printf("Please enter the cafeteria rating in between 1 and 10: ");
-             scanf("%d",&rating_array[i]);
-         }
-         total_rating+=rating_array[i];
-
-    }
-   return avg_rating(total_rating);
-  }*/
-
-void test_cafeteria(void) {
- CU_ASSERT_EQUAL(4.5,avg_rating(180));
-
+  /* Dummy fail*/
 }
-
